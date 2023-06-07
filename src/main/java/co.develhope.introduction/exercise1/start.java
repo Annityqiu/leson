351fcf2cccb990da553eabef441464a6d09c7ffe
Tom MBOYA <tommboya044@gmail.com>
2023-06-07 10:27:23 +0300

    1st one

---
+++ src/main/java/co.develhope.introduction/exercise1/start.java
@@ -0,0 +1,11 @@
+package co.develhope.introduction;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class start {
+
+       public static void main(String[] args) {
+           String birthday = "02/10/1997";
+           System.out.println("Hello develhope java student and your birthday is: " + birthday);
+       }
+   }
